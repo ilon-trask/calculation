d53ce5c873dfb2f252426fbdd92fd089a31daeef
ilon-trask <misha007zyt@gmail.com>
2024-01-14 15:04:34 +0200

    deleted font settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Калькуляція",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ua">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
